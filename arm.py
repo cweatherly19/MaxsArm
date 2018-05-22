d84e8848773b86e8238ae7c7fc2f560e0cb7ef4d
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-05-22 11:10:37 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,43 +1,37 @@
+#to access files needed to run the code
 import curses
 import setup
 import RoboPiLib as RPL
-
+#so define motor ports
 motor1 = 0
 motor2 = 1
+#to define the 'screen' in front of functions
 screen = curses.initscr()
-curses.cbreak()
-screen.keypad(1)
-
-screen.addstr("Enter q to quit. ")
-screen.addstr("Use the W, A, S, and D to move.")
-screen.refresh()
-
-#so is can ready what a key is
+#to tell the user valid inputs
+screen.addstr('Hit q to quit. Use the W, A, S, and D to test if code works. Detected key:')
+#to read key inputs
+position = 0
 key = ''
-#so if you hit 'q', the loop will end
+#to end loop if 'q' is hit
 while key != ord('q'):
     #so the key can be read
     key = screen.getch()
-    #so the code can read without hitting return
-    screen.refresh()
-    #so that you can read the keys and use the functions
-    while key == ord('w'):
+    screen.clear()
+    screen.addstr('Hit q to quit. Use the W, A, S, and D to test if code works. Detected key: ')
+    #to define what keys preform commands
+    if key == ord('w'):
+        screen.addstr('w key')
         RPL.servoWrite(motor1, 1000)
-        screen.refresh()
-        break
-    while key == ord('a'):
+    if key == ord('a'):
+        screen.addstr('a key')
         RPL.servoWrite(motor2, 3000)
-        screen.refresh()
-        break
-    while key == ord('s'):
-        RPL.servoWrite(motor1, 2000)
-        screen.refresh()
-        break
-    while key == ord('d'):
-        RPL.servoWrite(motor2, 800)
-        screen.refresh()
-        break
-    #so the robot will stop
-    RPL.servoWrite(motor1, 0)
-
+    if key == ord('s'):
+        screen.addstr('s key')
+        position = position + 50
+        RPL.servoWrite(motor1, position)
+    if key == ord('d'):
+        screen.addstr('d key')
+        position = position - 50
+        RPL.servoWrite(motor2, position)
+#to reformat the terminal/end the curses program
 curses.endwin()
