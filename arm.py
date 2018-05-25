412934d0bd90e7955a1b257a60314bfca09bae61
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-05-25 09:47:07 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -2,7 +2,6 @@
 import curses
 import setup
 import RoboPiLib as RPL
-import time
 #so define motor ports
 motor1 = 0
 motor2 = 1
@@ -12,7 +11,6 @@ screen = curses.initscr()
 screen.addstr('Hit q to quit. Use the W, A, S, and D to test if code works. Detected key:')
 #to read key inputs
 position = 1600
-duration = 0
 key = ''
 #to end loop if 'q' is hit
 while key != ord('q'):
@@ -23,26 +21,23 @@ while key != ord('q'):
     #to define what keys preform commands
     if key == ord('w'):
         screen.addstr('w key')
-        duration = time.time() + 0.5
         RPL.servoWrite(motor1, 1000)
     if key == ord('s'):
         screen.addstr('s key')
-        duration = time.time() + 0.5
         RPL.servoWrite(motor1, 2000)
     if key == ord('a'):
         screen.addstr('a key')
-        if position is 800:
+        if position < 800:
             position = 3000
         position = position - 50
         RPL.servoWrite(motor2, position)
     if key == ord('d'):
         screen.addstr('d key')
-        if position is 3000:
+        if position > 3000:
             position = 800
         position = position + 50
         RPL.servoWrite(motor2, position)
-    if duration - time.time() < 0:
-        RPL.servoWrite(motor1, 0)
+    RPL.servoWrite(motor1, 0)
 #to reformat the terminal/end the curses program
 curses.endwin()
 
